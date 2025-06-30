e67ca57ddc68a5a67c2cf3e9e92881848a0c6656
gaurav0973 <codejourneypanda@gmail.com>
2025-06-30 11:11:48 +0530

    added tge feature that after refreshing the page , I am not logging out

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,8 +1,29 @@
 import { Outlet } from "react-router-dom"
 import NavBar from "./components/NavBar"
 import Footer from "./components/Footer"
+import axios from "axios"
+import { useDispatch } from "react-redux"
+import { addUser } from "./utils/userSlice"
+import { useEffect } from "react"
 
 function Body() {
+
+  const dispatch = useDispatch()
+
+  const fetchUser = async () => {
+    try {
+
+      const res = await axios.get("http://localhost:7777/profile/view", { withCredentials: true })
+      dispatch(addUser(res.data.data))
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+  }
+
+
+  useEffect(()=>{
+    fetchUser()
+  }, [])
   return (
     <>
         <NavBar/>
