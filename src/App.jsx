d902e2a6eeb1bc8b3f60a84aabbd825cf372ae15
gaurav0973 <codejourneypanda@gmail.com>
2025-06-30 10:20:43 +0530

    added store in the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom"
 import Body from "./Body"
 import Login from "./pages/Login"
+import { Provider } from "react-redux"
+import appStore from "./utils/appStore.js"
 
 
 function App() {
   return (
     <>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Body/>}>
-            <Route path="/login" element={<Login/>}/>
-          
-          </Route>
-          
-        </Routes>
-      </BrowserRouter>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body/>}>
+              <Route path="/login" element={<Login/>}/>
+            
+            </Route>
+            
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   )
 }
